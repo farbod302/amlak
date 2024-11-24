5e63fd040c8d6b040b6dbe6a70419c32c07e3bab
farbod302 <f.aliakbari302@gmail.com>
2024-11-24 12:22:46 +0330

    add cert

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
+
+
+const https=require("https")
+const conf = {
+    key: fs.readFileSync("/etc/letsencrypt/live/netfan.orgr/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/netfan.orgr/fullchain.pem")
+}
+const server = https.createServer(conf, app);
+server.listen(port, () => { console.log("Server run on port " + port); })
+
 app.use(bodyParser.json({ extend: true }))
 require("dotenv").config()
 const mongo = require("mongoose")
 mongo.connect(process.env.DB)
 const port = process.env.PORT
-app.listen(port, () => { console.log("Server run on port " + port); })
 
 
 app.get("*", (req, res) => {
