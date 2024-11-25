6cc7be88ba8774b99530aa7a8244fea21cea43bb
farbod302 <f.aliakbari302@gmail.com>
2024-11-25 12:19:36 +0330

    fix options

--- container/messages.js
+++ container/messages.js
@@ -1,15 +1,15 @@
 const messages = {
     "get_phone": {
         text: `شماره تماس شما در سیستم ثبت نشده.لطفا شماره تماس خود را وارد کنید\nفرمت صحیح: \n 09125432121`,
-        option: {}
+        options: {}
     },
     "invalid_phone": {
         text: `شماره تماس نامعتبر\nفرمت صحیح: \n 09125432121`,
-        option: {}
+        options: {}
     },
     "phone_submitted": {
         text: `شماره تماس شما ثبت شد.اکنون می توانید از خدمات ربات استفاده کنید`,
-        option: {
+        options: {
             reply_markup: {
                 inline_keyboard: [
                     [{ text: 'ثبت آگهی', callback_data: 'submit_new_file' }],
