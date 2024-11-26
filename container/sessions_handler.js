19b2b74151d66d716f6cb50e49b2732e61db1e5d
farbod302 <f.aliakbari302@gmail.com>
2024-11-26 11:45:54 +0330

    sd

--- container/sessions_handler.js
+++ container/sessions_handler.js
@@ -4,15 +4,15 @@ const sessions_handler = {
     create_session({ user_id, session_type }) {
         const session_id = uid(8)
         const new_session = { user_id, session_id, session_type, finish: false, date: Date.now() }
-        const json_str = fs.readFileSync("./sessions.json")
+        const json_str = fs.readFileSync(`${__dirname}/sessions.json`)
         let json = JSON.parse(json_str.toString())
         json = json.filter(e => e.user_id !== user_id)
         json.push(new_session)
-        fs.writeFileSync("./sessions.json", JSON.stringify(json))
+        fs.writeFileSync(`${__dirname}/sessions.json`, JSON.stringify(json))
         return session_id
     },
     edit_session({ user_id, data }) {
-        const json_str = fs.readFileSync("./sessions.json")
+        const json_str = fs.readFileSync(`${__dirname}/sessions.json`)
         const json = JSON.parse(json_str.toString())
         const session = json.find(e => e.user_id === user_id)
         if (!session) return false
@@ -21,7 +21,7 @@ const sessions_handler = {
         keys.forEach(e => prv_data[e] = data[e])
         const new_json = json.filter(e => e.user_id !== user_id)
         new_json.push(prv_data)
-        fs.writeFileSync("./sessions.json", JSON.stringify(new_json))
+        fs.writeFileSync(`${__dirname}/sessions.json`, JSON.stringify(new_json))
         return prv_data
     },
 
