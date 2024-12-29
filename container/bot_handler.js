45d34ab5b040a930339f3e665253242cf8fc9bff
farbod302 <f.aliakbari302@gmail.com>
2024-12-29 11:06:26 +0330

    sdf

--- container/bot_handler.js
+++ container/bot_handler.js
@@ -277,6 +277,7 @@ const bot_handler = {
                     }
                     for (const f in files) {
                         const { info, areas, city, address, images } = f
+                        console.log({images});
                         const images_to_send = images.length ? images : ["https://static.vecteezy.com/system/resources/previews/022/059/000/non_2x/no-image-available-icon-vector.jpg"]
                         type_finder(f)
                         msg += `واقع در شهر ${city}\nآدرس: ${address}\nمنطقه: ${areas}\nتوضیحات: ${info}`
